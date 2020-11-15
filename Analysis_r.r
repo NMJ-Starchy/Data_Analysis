5b5ce074302e491f95961cb5debe1746bb945a54
NMJ-Starchy <joh16036@byui.edu>
2020-11-14 21:13:08 -0700

    Update Analysis

--- Analysis_r.r
+++ Analysis_r.r
@@ -4,10 +4,22 @@ library(pander)
 
 ramen_data <- read_csv('/Users/natejhome/Documents/Coding Tools/CS 246/Data Analysis/ramen-ratings.csv')
 
+# Calculates the average ramen rating by style
 ramen_data %>%
     group_by(Style) %>%
-    summarise(Average_Rating = mean(Stars))
+    summarize(Average_Rating = mean(Stars))
+
+# Calculates the average ramen rating by country
+ramen_data %>%
+    group_by(Country) %>%
+    summarize(Average_Rating = mean(Stars))
+
+# Plot the amount of Ramen products made by country
+ggplot(ramen_data, mapping = aes(x = Country, color = Country, fill = Country)) +
+    geom_histogram(stat = 'count') +
+    ylab('Number of Ramen Products')
 
 
+# Plot the Star ratings of the different ramen styles
 ggplot() +
-geom_point(ramen_data, mapping = aes(x = Style, y = Stars))
+geom_point(ramen_data, mapping = aes(x = Style, y = Stars, color = Country))
